5dcfcd130b71f5ce6b9982346d9e0dc63c1f323c
Mohammed Zayan <zayanit@gmail.com>
2021-06-22 22:43:17 +0300

    Add completed column to todos table
    Lesson6: Migrations - Flask-Migrate - Part 3

--- todoapp/app.py
+++ todoapp/app.py
@@ -17,6 +17,7 @@ class Todo(db.Model):
 	__tablename__ = 'todos'
 	id = db.Column(db.Integer, primary_key=True)
 	description = db.Column(db.String(), nullable=False)
+	completed = db.Column(db.Boolean, nullable=False, default=False)
 
 	def __repr__(self):
 		return f'<TODO {self.id} {self.description}>'
